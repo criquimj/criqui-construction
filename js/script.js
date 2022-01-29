83d8a03f09fde732ce4497a6435f8f7640eb2d46
Matthew Criqui <mcriqui@Matthews-MacBook-Pro.local>
2022-01-29 13:01:20 -0600

    updated stickyNav function to not pollute global namespace

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 console.log("testing connection");
 
-const headerSection = document.querySelector(".header");
+const stickyNav = () => {
+  const headerSection = document.querySelector(".header");
+  const obs = new IntersectionObserver(
+    (entries) => {
+      const ent = entries[0];
+      console.log(ent);
+      const navBar = document.querySelector(".nav");
 
-const obs = new IntersectionObserver(
-  (entries) => {
-    const ent = entries[0];
-    console.log(ent);
-    const navBar = document.querySelector(".nav");
+      !ent.isIntersecting
+        ? navBar.classList.add("nav--scrolling")
+        : navBar.classList.remove("nav--scrolling");
+    },
+    { root: null, threshold: 0, rootMargin: "-200px" }
+  );
+  obs.observe(headerSection);
+};
 
-    !ent.isIntersecting
-      ? navBar.classList.add("nav--scrolling")
-      : navBar.classList.remove("nav--scrolling");
-  },
-  { root: null, threshold: 0, rootMargin: "-200px" }
-);
-
-obs.observe(headerSection);
+stickyNav();
