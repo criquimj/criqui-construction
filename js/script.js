0b05b05f2bc516e9257caae0207ae72fb5df1216
Matthew Criqui <mcriqui@Matthews-MacBook-Pro.local>
2022-01-30 14:44:15 -0600

    adding functionality for switching between services in service setion

--- js/script.js
+++ js/script.js
@@ -14,3 +14,45 @@ const stickyNav = () => {
   observer.observe(headerSection);
 };
 stickyNav();
+
+// SERVICES SECTION
+
+const excButton = document.querySelector("#exc-btn");
+const demButton = document.querySelector("#dem-btn");
+const envButton = document.querySelector("#env-btn");
+const railButton = document.querySelector("#rail-btn");
+
+const allServiceButtons = [excButton, demButton, envButton, railButton];
+const servicesDisplay = document.querySelector(".services__display");
+const gradient =
+  "linear-gradient(rgba($color-black, 0.7),rgba($color-black, 0.7))";
+// const displayBackground = servicesDisplay.style.backgroundImage;
+console.log(window.getComputedStyle(servicesDisplay, false));
+
+// servicesDisplay.style.backgroundImage = `linear-gradient(
+//   to left top,
+//   rgba($color-black, 0.7),
+//   rgba($color-black, 0.7)
+// ),
+// url(./img/dem-btn.jpeg)`;
+
+allServiceButtons.map((btn) =>
+  btn.addEventListener("click", () => {
+    let backgroundImage = `url(./img/${btn.id}.jpeg)`;
+    servicesDisplay.style.backgroundImage = backgroundImage;
+
+    const selectedButton = document.querySelector(`#${btn.id}`);
+
+    const nonSelectedButtons = allServiceButtons.filter(
+      (btn) => btn.id !== selectedButton.id
+    );
+
+    if (!selectedButton.classList.contains("services__btn--active")) {
+      selectedButton.classList.toggle("services__btn--active");
+
+      nonSelectedButtons.map((nonBtn) =>
+        nonBtn.classList.remove("services__btn--active")
+      );
+    }
+  })
+);
