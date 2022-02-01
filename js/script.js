6cff49d94f4e9c1ae41fd0871fae8ee87baa5532
Matthew Criqui <mcriqui@Matthews-MacBook-Pro.local>
2022-02-01 09:40:47 -0600

    finalizing mobile nav styles

--- js/script.js
+++ js/script.js
@@ -320,3 +320,26 @@ allServiceButtons.map((btn) =>
     displayOverlay.insertAdjacentHTML("afterbegin", htmlContent);
   })
 );
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) =>
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("header__main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  })
+);
